17ac7f3f9ded4ce71b6fbe121044ee437c3557f2
hhinkel <ad-astra@comcast.net>
2019-10-13 17:25:14 -0400

    Tablet changes

--- app/src/main/java/com/example/eventtimerstart/MainActivity.java
+++ app/src/main/java/com/example/eventtimerstart/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -246,6 +247,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        showSoftNumPad(dialogView);
+    }
+
+    public void showSoftNumPad(View view) {
+        if (view.requestFocus()) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
     }
 
     private String createMessageString (Rider rider) {
